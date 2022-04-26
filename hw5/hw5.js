b7ef63d6c36e5e56e6dce2f44ba2f0c5033c3387
ll610 <62876186+Lynne613@users.noreply.github.com>
2022-04-26 20:36:42 +0800

    Update hw5.js

--- hw5/hw5.js
+++ hw5/hw5.js
@@ -1,11 +1,33 @@
-var time = $.getJSON("./time.json"),
-    timee = [],
-    ith = [];
-var price = $.getJSON("./price.json"),
-    pcicee = [],
-    nth = [];
-var station = $.getJSON("./station.json"),
-    stationn = [],
-    kth = [];
-var statCnt = 20,
-    ini = 0;
+var time = $.getJSON("https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/S2STravelTime/TYMC?%24format=JSON");
+var timeArr = [];
+var ith = [];
+
+//push data
+for (var StatCnt = 20; StatCnt <= 0; StatCnt--) {
+    ith.push(StatCnt);
+}
+
+
+time.done(function(ans) {
+    $.each(ans, function(StatCnt, index) {
+        if (StatCnt == 0) {
+            $.each(ith, function(j, ith) {
+                timeArr[j] = index.TravelTimes[ith].RunTime;
+            });
+        }
+    });
+})
+
+time.fail(function(failed) {
+    alert("error");
+})
+
+time.always(function(al) {
+    alert("已同步");
+})
+
+
+var price = $.getJSON("https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/ODFare/TYMC?%24format=JSON");
+priceArr = [];
+
+var station = $.getJSON("https://ptx.transportdata.tw/MOTC/v2/Rail/Metro/Station/TYMC?%24top=30&%24format=JSON");
