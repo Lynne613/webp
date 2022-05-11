599d93fbd7219e0a35d28236da9b475c3932a976
ll610 <62876186+Lynne613@users.noreply.github.com>
2022-05-11 16:47:17 +0800

    Add files via upload

---
+++ qz8/src/HelloCGU.js
@@ -0,0 +1,8 @@
+const styleArgument = { fontSize: "100px", color: "red" };
+
+const HelloCGU = () => {
+    var Hello = < h5 style = { styleArgument } > hello CGU!! < /h5>
+    return Hello;
+}
+
+export default HelloCGU;
