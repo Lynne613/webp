e4aa01c3e1baa43b9603e985cf1a11fa5b3482ad
Lynne <62876186+Lynne613@users.noreply.github.com>
2022-06-22 01:04:28 +0800

    Add files via upload

---
+++ webp_v1/src/components/Navbarcomponent/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Menu , Button } from 'semantic-ui-react'
+
+
+export default class Navbarcomponent extends Component {
+  state = {}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+   
+
+    return (
+      <Menu stackable>
+        
+        <Menu.Item
+          name='Photocon'
+          >
+          Photocon 
+        </Menu.Item>
+
+        <Menu.Item>
+        <img className="ui avatar image" src="https://react.semantic-ui.com/images/avatar/large/elliot.jpg" />             
+        </Menu.Item>
+
+ 
+      </Menu>
+    )
+  }
+}
